7de53c37afca4eefd3c9e28d6646e975af02aaf7
Mauricio Rodriguez <maurodri@student.42sp.org.br>
2023-10-27 20:39:37 -0300

    clearing buffer impl

--- get_next_line.c
+++ get_next_line.c
@@ -6,50 +6,53 @@
 /*   By: maurodri <maurodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 21:17:35 by maurodri          #+#    #+#             */
-/*   Updated: 2023/10/27 20:30:49 by maurodri         ###   ########.fr       */
+/*   Updated: 2023/10/27 20:59:45 by maurodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	buffer_flush(t_buffer *buffer, t_stringbuilder builder)
-{
-	while (buffer->is_init && builder && buffer->i < buffer->char_read)
-	{
-		builder = stringbuilder_addchar(builder, buffer->arr[buffer->i]);
-		if (buffer->arr[buffer->i++] == '\n')
-			break ;
-	}
-}
-
 char	*get_next_line(int fd)
 {
-	static t_buffer	buffer;
+	static t_buffer	b;
 	t_stringbuilder	builder;
 
+	b.char_read = 1;
 	builder = stringbuilder_new();
-	while (builder && (buffer.char_read > 0 || !buffer.is_init))
+	while (builder && (b.char_read > 0 || !b.is_init))
 	{
-		buffer_flush(&buffer, builder);
-		if (buffer.i >= buffer.char_read || !buffer.is_init)
+		while (b.i < BUFFER_SIZE && b.is_init)
 		{
-			buffer.is_init = 1;
-			buffer.i = 0;
-			buffer.char_read = read(fd, buffer.arr, BUFFER_SIZE);
+			if (b.arr[b.i] != '\0')
+			{
+				builder = stringbuilder_addchar(builder, b.arr[b.i]);
+				if (b.arr[b.i] == '\n')
+				{
+					b.arr[b.i] = '\0';
+					break ;
+				}
+				b.arr[b.i] = '\0';
+			}
+			b.i++;
+		}
+		if (b.i >= BUFFER_SIZE || !b.is_init)
+		{
+			b.char_read = read(fd, b.arr, BUFFER_SIZE);
+			b.i = 0;
+			b.is_init = 1;
 		}
 		else
 			break ;
 	}
 	if (!builder)
 		return (NULL);
-	if (buffer.char_read < 0 || stringbuilder_isempty(builder))
+	if (b.char_read < 0 || stringbuilder_isempty(builder))
 	{
 		stringbuilder_destroy(builder);
 		return (NULL);
 	}
 	return (stringbuilder_build(builder));
 }
-
 /*
 #include <fcntl.h>
 #include <stdio.h>
@@ -70,4 +73,4 @@ int	main(void)
 	printf("==END==\n");
 	return (0);
 }
-*/ 
+*/
